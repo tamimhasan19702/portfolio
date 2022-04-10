3d1e78f7d59184b9eebcb3cb88ccce46ff68b944
tamimhasan19702 <tamimhasancu@gmail.com>
2022-04-11 01:48:26 +0600

    c

--- style.js
+++ style.js
@@ -9,5 +9,26 @@
 
  window.addEventListener('scroll', scrollHeader)
 
+// ------------------------------------------------------------
 
- 
+const modalViews= document.querySelectorAll('.services-modal'),
+      modalBtns = document.querySelectorAll('.services-button'),
+      modalClose = document.querySelectorAll('.services-modal-close')
+
+      let modal = function(modalClick){
+          modalViews[modalClick].classList.add('active-modal')
+      }
+
+      modalBtns.forEach((mb, i) => {
+          mb.addEventListener('click' , () => {
+              modal(i)
+          })
+      })
+
+      modalClose.forEach((mc) => {
+          mc.addEventListener('click' , () => {
+              modalViews.forEach((mv) => {
+                  mv.classList.remove('active-modal')
+              })
+          })
+      })
