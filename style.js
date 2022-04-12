198000633b6227f9782f5b25f0f6da841b0c4437
tamimhasan19702 <tamimhasancu@gmail.com>
2022-04-12 20:38:06 +0600

    c

--- style.js
+++ style.js
@@ -32,3 +32,78 @@ const modalViews= document.querySelectorAll('.services-modal'),
               })
           })
       })
+
+
+      const linkWork = document.querySelectorAll('.work-item')
+
+      function activeWork(){
+          linkWork.forEach(l=> l.classList.remove('active-work'))
+          this.classList.add('active-work')
+      }
+
+      linkWork.forEach(l=> l.addEventListener('click', activeWork));
+
+
+    //   --------------------------------------------------------------
+
+    
+let pagination = $('.pagination');
+
+function setPagination(){
+   pagination.jPages({
+     containerID: 'container',
+     perPage: 4,
+     startPage: 1,
+     startRange: 1,
+     midRange: 3,
+     endRange: 1,
+     first: false,
+     last: false
+   });
+}
+
+function destroyPagination() {
+  pagination.jPages('destroy');
+};
+
+setPagination();
+
+$('#container').mixItUp({
+  callbacks: {
+    onMixLoad: function(state,futureState ){
+      console.log('mix Loaded');
+      //setPagination();
+    },
+    onMixStart: function(state,futureState ){
+      destroyPagination();
+    },
+    onMixEnd: function(state, futureState){
+      console.log('mix End');
+      setPagination();
+    }
+  }
+});
+
+// ------------------------------------------------------------------
+
+let swiperTestimonial = new Swiper(" .testimonial-container",{
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+
+  pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+    slidesPerView: 2,
+    spaceBetween: 48,
+    },
+
+  },
+
+});
